741b3f60a919975ad51cc320fc1c536b162592a4
billy_ho <billy_ho@paradise-soft.com.tw>
2020-10-29 12:34:30 +0800

    only health

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 const (
@@ -14,44 +11,44 @@ const (
 )
 
 func main() {
-	log.Println("init line bot")
-	bot, err := linebot.New(secret, token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	http.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte("^_^"))
 	})
 
-	log.Println("set line bot callback func")
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
-		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				case *linebot.StickerMessage:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
-			}
-		}
-	})
+	//log.Println("init line bot")
+	//bot, err := linebot.New(secret, token)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println("set line bot callback func")
+	//http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+	//	events, err := bot.ParseRequest(req)
+	//	if err != nil {
+	//		if err == linebot.ErrInvalidSignature {
+	//			w.WriteHeader(400)
+	//		} else {
+	//			w.WriteHeader(500)
+	//		}
+	//		return
+	//	}
+	//	for _, event := range events {
+	//		if event.Type == linebot.EventTypeMessage {
+	//			switch message := event.Message.(type) {
+	//			case *linebot.TextMessage:
+	//				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+	//					log.Print(err)
+	//				}
+	//			case *linebot.StickerMessage:
+	//				replyMessage := fmt.Sprintf(
+	//					"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+	//				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+	//					log.Print(err)
+	//				}
+	//			}
+	//		}
+	//	}
+	//})
 
 	log.Print("serve port: 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
