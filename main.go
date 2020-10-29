9e827497d6af37d440471a832dca256a63b32b39
billy_ho <billy_ho@paradise-soft.com.tw>
2020-10-29 14:22:52 +0800

    line bot via gin

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 const (
 	secret = "2bf8fa7ecd6a10f7d467b380040653e2"
 	token  = "mP9ehjSoLoFo7PHY9O0qqkCerXfM7tKTVRhziAz0X2LLgxpiM3hOqTeY2Lp7arBDH6Pb/AocexieMVttnXEtlonHGFUsa5ehceyby6JH2rHhmGplA7/CjryhpTi7UNT8Aku89faTS2Eq8LywsQoolwdB04t89/1O/w1cDnyilFU="
 )
+
+var botInstance *linebot.Client
+
+func init() {
+	InitLineBot()
+}
 
 func main() {
 	defer func() {
@@ -22,43 +30,54 @@ func main() {
 	}()
 
 	router := gin.Default()
+
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "^_^")
 	})
-	router.Run()
+	router.GET("/line/init", func(ctx *gin.Context) {
+		err := InitLineBot()
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, err)
+		} else {
+			ctx.String(http.StatusOK, "ok")
+		}
+	})
+	router.GET("/callback", func(ctx *gin.Context) {
+		req := ctx.Request
+		events, err := botInstance.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				ctx.JSON(http.StatusBadRequest, err)
+			} else {
+				ctx.JSON(http.StatusInternalServerError, err)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = botInstance.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = botInstance.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
 
-	//log.Println("init line bot")
-	//bot, err := linebot.New(secret, token)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//log.Println("set line bot callback func")
-	//http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-	//	events, err := bot.ParseRequest(req)
-	//	if err != nil {
-	//		if err == linebot.ErrInvalidSignature {
-	//			w.WriteHeader(400)
-	//		} else {
-	//			w.WriteHeader(500)
-	//		}
-	//		return
-	//	}
-	//	for _, event := range events {
-	//		if event.Type == linebot.EventTypeMessage {
-	//			switch message := event.Message.(type) {
-	//			case *linebot.TextMessage:
-	//				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-	//					log.Print(err)
-	//				}
-	//			case *linebot.StickerMessage:
-	//				replyMessage := fmt.Sprintf(
-	//					"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-	//				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-	//					log.Print(err)
-	//				}
-	//			}
-	//		}
-	//	}
-	//})
+	router.Run()
+}
+
+func InitLineBot() error {
+	log.Println("init line bot")
+	var err error
+	botInstance, err = linebot.New(secret, token)
+
+	return err
 }
